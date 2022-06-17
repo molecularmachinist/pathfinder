889c9040346fccb6fe63e7f43aaaf21318ce416c
Vanilja Hyppönen <vanilja.hypponen@helsinki.fi>
2022-06-17 11:20:21 +0300

    Created some production scripts

---
+++ production/prod2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH --output=prod_2.txt
+#SBATCH --time=00:15:00
+#SBATCH --job-name=prod2
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --account=project_2006125
+#SBATCH --partition=medium
+
+module load gromacs
+
+istep=step7_2
+pstep=step7_1
+rest_prefix=step5_input
+prod_prefix=step7_production
+
+srun gmx_mpi grompp -f ${prod_prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -t ${pstep}.cpt -p topol.top -n index.ndx
+srun gmx_mpi mdrun -v -deffnm ${istep}
