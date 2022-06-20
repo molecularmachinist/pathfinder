621851945e7a04368e24e5e92b5579f81c1e0cf6
Vanilja Hyppönen <vanilja.hypponen@helsinki.fi>
2022-06-20 15:35:13 +0300

    Updated step7.sh script and step7 mdp file

--- equilibration/step7.sh
+++ equilibration/step7.sh
@@ -1,19 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 #SBATCH --output=step7.txt
-#SBATCH --time=02:00:00
+#SBATCH --time=26:00:00
 #SBATCH --job-name=step7
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=64
-#SBATCH --mem=40G
+#SBATCH --mem=12G
 #SBATCH --account=project_2006125
 #SBATCH --partition=medium
 
 module load gromacs
 
-istep=step7_1
-pstep=step6.6_equilibration
-rest_prefix=step5_input
-prod_prefix=step7_production
 
-srun gmx_mpi grompp -f ${prod_prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -p topol.top -n index.ndx
-srun gmx_mpi mdrun -v -deffnm ${istep}
+srun gmx_mpi grompp -f step7_production.mdp -o step7.tpr -c step6.6_equilibration.gro -p topol.top -n index.ndx -maxwarn 1
+srun gmx_mpi mdrun -v -deffnm step7
