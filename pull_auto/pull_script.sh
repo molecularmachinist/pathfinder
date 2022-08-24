9f2c829053a7479f2f07de67fd637ebea82a9854
Vanilja Hyppönen <vanilja.hypponen@helsinki.fi>
2022-08-24 11:37:09 +0300

    Modified new_K functions

--- pull_auto/pull_script.sh
+++ pull_auto/pull_script.sh
@@ -37,7 +37,7 @@ K_MIN_ORIG=5                            #some starting values for K
 K_MAX_ORIG=100
 K_MIN=5
 K_MAX=100
-K_MID=$((($K_MAX - $K_MIN)/2))
+K_MID=$(( $K_MIN + ($K_MAX - $K_MIN)/2 ))
 K_MID=$(( (($K_MID+2)/5)*5 ))
 K2=$((($K_MID - $K_MIN)/2))
 K2=$(( (($K2+2)/5)*5 ))
@@ -106,36 +106,58 @@ status () {
 new_K_3 () {
     if [[ STATUS_ARRAY[0] -eq 1 ]]             #if min K was successful
     then
-        done
+        echo "K_MIN=${K_ARRAY[0]} was successful"
+        break
     elif [[ STATUS_ARRAY[1] -eq 1 ]]           #if middle K was successful
     then
+        echo "K_MID=${K_ARRAY[1]} was successful"
         K_MAX=$K_MID                           #previous middle value is now max value
-        K_MID=$(( ($K_MID - $K_MIN)/2 ))       #new middle value is between old mid and min
+        K_MID=$(( $K_MIN + ($K_MAX - $K_MIN)/2 ))       #new middle value is between old mid and min
         K_MID=$(( (($K_MID+2)/5)*5 ))          #rounded to the nearest multiple of 5
-        STATUS_ARRAY=(0 0 1)
+        K_ARRAY=($K_MIN $K_MID $K_MAX)
+        declare -A STATUS_ARRAY
+        STATUS_ARRAY[0]=0
+        STATUS_ARRAY[1]=0
+        STATUS_ARRAY[2]=1
     elif [[ STATUS_ARRAY[2] -eq 1 ]]           #if max K was successful
     then
+        echo "K_MAX=${K_ARRAY[2]} was successful"
         K_MIN=$K_MID                           #previous mid value is now min value
-        K_MID=$(expr ($K_MAX - $K_MID)/2)      #new middle value is between max and old mid
+        K_MID=$(( $K_MIN + ($K_MAX - $K_MIN)/2 ))      #new middle value is between max and old mid
         K_MID=$(( (($K_MID+2)/5)*5 ))          #roundend to the nearest multiple of 5
-        STATUS_ARRAY=(0 0 1)
+        K_ARRAY=($K_MIN $K_MID $K_MAX)
+        declare -A STATUS_ARRAY
+        STATUS_ARRAY[0]=0
+        STATUS_ARRAY[1]=0
+        STATUS_ARRAY[2]=1
     fi
-    echo "New K is $K_MID"
+    echo "New K_MID is $K_MID"
+    echo "New values of K are: ${K_ARRAY[*]}"
 }
 
 #New function for parallel version for determining a new K after running 5 pulling sims
 #Assumes the STATUS_ARRAY has 5 values
-new_K_5 () {
-    for i in "${K_array[@]}"
+new_K_5 () {                                               
+    for i in {0..4}
     do
+        echo "Checking K=${K_ARRAY[$i]}"
         if [[ ${STATUS_ARRAY[$i]} -eq 1 ]]
         then
-            K_MAX=$i
-            K_MIN=$i-1
-            K_MID=$(expr ($K_MAX - $K_MIN)/2)
+            echo "K=${K_ARRAY[$i]} was successful"
+            K_MAX=${K_ARRAY[$i]}
+            local prev=$(( $i - 1 ))
+            K_MIN=${K_ARRAY[$prev]}
+            K_MID=$(( $K_MIN + ($K_MAX - $K_MIN)/2 ))
             K_MID=$(( (($K_MID+2)/5)*5 ))
-            STATUS_ARRAY=(0 0 1)
+            declare -A STATUS_ARRAY
+            STATUS_ARRAY[0]=0
+            STATUS_ARRAY[1]=0
+            STATUS_ARRAY[2]=1
+            K_ARRAY=($K_MIN $K_MID $K_MAX)
+            echo "New values of K are: ${K_ARRAY[*]}"
             break
+        else
+            echo "K=${K_ARRAY[$i]} was not successful"
         fi
     done        
 }
@@ -209,7 +231,7 @@ run_simulation () {
     do
         K_MIN=$K_MIN_ORIG                   #set 5 different evenly spaced Ks 
         K_MAX=$K_MAX_ORIG                   
-        K_MID=$((($K_MAX - $K_MIN)/2))
+        K_MID=$(( $K_MIN + ($K_MAX - $K_MIN)/2 ))
         K_MID=$(( (($K_MID+2)/5)*5 ))
         K2=$((($K_MID - $K_MIN)/2))
         K2=$(( (($K2+2)/5)*5 ))
