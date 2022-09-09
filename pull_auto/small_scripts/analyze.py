cc945a8d0084dad6add939cdec36a982bed75eab
hypponenvanilja <vanilja@ad.helsinki.com>
2022-09-09 13:43:57 +0300

    added RMSD plotter

--- pull_auto/small_scripts/analyze.py
+++ pull_auto/small_scripts/analyze.py
@@ -10,15 +10,26 @@ import sys
 import os
 
 
-file=sys.argv[1]
+#file=sys.argv[1]
+file="rmsd.xvg"
 x,y = np.loadtxt(file,comments=["@","#"],unpack=True)
 n=math.ceil(0.8*len(x))
 x=x[-n:]
 y=y[-n:]
 slope=linregress(x,y).slope
 slope=float('{:f}'.format(slope))
+print("Slope:", slope)
 if slope < 0.25 and slope > -0.25:
     res=1
 else:
     res=0
-print(res)
+print("RES: ", res)
+figure = plt.figure(figsize=(6,3))
+ax = figure.add_subplot(111)
+ax.plot(x, y)
+ax.set_xlim(x[0], x[-1])
+ax.set_ylim(0, 2)
+ax.set_xlabel("Time (ns)")
+ax.set_ylabel("RMSD (Å)")
+figure.tight_layout()
+plt.show()
