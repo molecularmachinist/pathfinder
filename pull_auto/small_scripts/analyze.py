d74e3116189e58379dfa99a743e8d35aa0c927e8
hypponenvanilja <vanilja@ad.helsinki.com>
2022-09-15 12:56:49 +0300

    run_eq updated

--- pull_auto/small_scripts/analyze.py
+++ pull_auto/small_scripts/analyze.py
@@ -1,29 +1,30 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
+import sys
+import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy.stats import linregress
 import math
-import sys
-import os
 
 
-#file=sys.argv[1]
-file="rmsd.xvg"
+
+file=sys.argv[1]
+domain=sys.argv[2]
+#file="rmsd.xvg"
 x,y = np.loadtxt(file,comments=["@","#"],unpack=True)
 n=math.ceil(0.8*len(x))
 x=x[-n:]
 y=y[-n:]
 slope=linregress(x,y).slope
 slope=float('{:f}'.format(slope))
-print("Slope:", slope)
+#print("Slope:", slope)
 if slope < 0.25 and slope > -0.25:
     res=1
 else:
     res=0
-print("RES: ", res)
 figure = plt.figure(figsize=(6,3))
 ax = figure.add_subplot(111)
 ax.plot(x, y)
@@ -31,5 +32,7 @@ ax.set_xlim(x[0], x[-1])
 ax.set_ylim(0, 2)
 ax.set_xlabel("Time (ns)")
 ax.set_ylabel("RMSD (Å)")
+ax.set_title("RMSD: Equilibration of {} domain".format(domain))
 figure.tight_layout()
 plt.show()
+print(res)
--- pull_auto/small_scripts/run_eq.sh
+++ pull_auto/small_scripts/run_eq.sh
@@ -4,10 +4,10 @@ run_eq() {
     #gmx_mpi rms -s pull_eq_${DOMAIN}${ITERATION}.tpr -f pull_eq_${DOMAIN}${ITERATION}.trr -o pull_eq_${DOMAIN}${ITERATION}_rmsd.xvg -tu ns
     #echo "backbone backbone"
 
+    DOMAIN=TK
     FILE="rmsd.xvg"
-    echo "HI"
-    #RESULT=$(/usr/bin/env python3 analyze.py $FILE)      #0 means fail, the slope wasnt close enough to 0
-    RESULT=0
+    RESULT=$(/usr/bin/env python3 analyze.py $FILE $DOMAIN)      #0 means fail, the slope wasnt close enough to 0
+    echo "RESULT: $RESULT" 
     if [[ $RESULT -eq 0 ]]                               #1 means success, the slope was close enough to 0  
     then
         echo "The equilibration wasn't successful. The structure isn't equilibrated enough."
@@ -23,3 +23,5 @@ run_eq() {
         echo "The equilibration was successful."
     fi
 }
+
+run_eq
